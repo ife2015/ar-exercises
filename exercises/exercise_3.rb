9566a22de3993e1676290877d1c7154b058c3f5c
Ife Olaifa <ifeolaifa5@gmail.com>
2020-11-10 02:15:42 +0000

    excercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,8 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(name: "Gastown")
+@store3.destroy
+
+
+puts "There are #{Store.all.count} store in the table"
